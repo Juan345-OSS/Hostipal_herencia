a8091f6d5b6e318e3be0bb06c5106d68302218e3
Juan345-OSS <camilo20050707@gmail.com>
2026-07-14 18:26:15 -0500

    Trabajo terminado

--- Lib/Models/hospital.dart
+++ Lib/Models/hospital.dart
@@ -1,6 +1,7 @@
 import 'Personal.dart';
 import 'medico.dart';
 import 'enfermeros.dart';
+import 'dart:math';
 
 class Hospital {
     List<Personal> trabajadores = [];
@@ -52,7 +53,82 @@ class Hospital {
         }
     }
 
-    void aumentarSalario(){}
+    void aumentarSalario() {
+    for (var trabajador in trabajadores) {
+        double salarioActual = trabajador.getSalario();
+        double nuevoSalario = salarioActual;
+
+    if (salarioActual < 3000000) {
+      nuevoSalario = salarioActual * 1.12;
+    } else if (salarioActual >= 3000000 && salarioActual <= 5000000) {
+      nuevoSalario = salarioActual * 1.08;
+    } else if (salarioActual > 5000000) {
+      nuevoSalario = salarioActual * 1.05;
+    }
+
+    trabajador.setSalario(nuevoSalario);
+    print("Nuevo salario de ${trabajador.getNombre()}: $nuevoSalario");
+    }
+    }
+
+    void contarTrabajadores(){
+        int medicos = 0;
+        int enfermeros = 0;
+
+        for (var i = 0; i < trabajadores.length; i++){
+            if (trabajadores[i] is Medico) {
+                medicos= i + medicos;
+            }else if(trabajadores[i] is Enfermeros){
+                enfermeros= i + enfermeros;
+            }
+        }
+        var total= medicos + enfermeros;
+        print('El total de medicos son $medicos y el de enfermeros es $enfermeros , Total: $total');
+    }
+
+    void mostrarMayorSalario() {
+        if (trabajadores.isEmpty) {
+        print("No hay trabajadores registrados");
+        return;
+    }
+
+    Personal mayor = trabajadores[0]; // asumimos el primero como referencia
+
+    for (var trabajador in trabajadores) {
+      if (trabajador.getSalario() > mayor.getSalario()) {
+      mayor = trabajador;
+      }
+    }
+
+  print("\n===== Trabajador con mayor salario =====");
+  mayor.mostrarInformacion();
+  }
+
+  void otorgarBono() {
+  for (var trabajador in trabajadores) {
+    if (trabajador is Medico) {
+      if (trabajador.getPacientesAtendidos() > 100) {
+        double salarioActual = trabajador.getSalario();
+        double nuevoSalario = salarioActual + 800000;
+
+        trabajador.setSalario(nuevoSalario);
+        print('Salario actual era $salarioActual. Se otorgó un bono de 800000. Nuevo salario: $nuevoSalario');
+      }
+    }
+
+    if (trabajador is Enfermeros) {
+      if (trabajador.getHabitacionesAsignadas() > 25) {
+        double salarioActual = trabajador.getSalario();
+        double nuevoSalario = salarioActual + 500000;
+
+        trabajador.setSalario(nuevoSalario);
+        print('Salario actual era $salarioActual. Se otorgó un bono de 500000. Nuevo salario: $nuevoSalario');
+      }
+    }
+  }
+}
+
+
 
 
 }
